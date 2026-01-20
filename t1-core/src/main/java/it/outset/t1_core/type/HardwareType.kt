7052004a44f51802cb62e34187a287eb58fd2159
Antonio Moro <antonio.moro@on-divisions.com>
2026-01-20 17:52:08 +0100

    Aggiunta la funzionalità di tara per i container. Introdotta la `ContainerTareBottomSheet` per inserire e catturare la tara di uno o due container. Aggiunta la logica di calcolo del peso e test unitari in `MeasureBase` per la gestione della tara del container. Aggiornate le stringhe di traduzione e aggiunta un'opzione di menu per la tara. Refactoring dei nomi delle versioni hardware in `HardwareType.kt`. Rimosse le icone `ic_menu_save` e i file di menu non utilizzati.

--- t1-core/src/main/java/it/outset/t1_core/type/HardwareType.kt
+++ t1-core/src/main/java/it/outset/t1_core/type/HardwareType.kt
@@ -9,22 +9,22 @@ enum class HardwareType(
     val isNodeOnly: Boolean
 ) {
     T1("SFPN001", 4, 15.toByte(), 4, 2, false),
-    TX4("T008.02", 4, 15.toByte(), 4, 2, false),
-    T011_01("T011.01", 4, 15.toByte(), 4, 2, false),
-    T011_10("T011.10", 4, 15.toByte(), 4, 2, false),
-    K008_01("K008.01", 4, 15.toByte(), 4, 2, false),
-    K008_10("K008.10", 4, 15.toByte(), 4, 2, false),
-    K009_00("K009.00", 8, 255.toByte(), 4, 2, false),
-    K009_10("K009.10", 8, 255.toByte(), 4, 2, false),
-    K006_10("K006.10", 8, 255.toByte(), 4, 2, true),  // Prototype only
-    K006_20("K006.20", 8, 255.toByte(), 4, 2, true);
+    TX4("T008|02", 4, 15.toByte(), 4, 2, false),
+    T011_01("T011|01", 4, 15.toByte(), 4, 2, false),
+    T011_10("T011|10", 4, 15.toByte(), 4, 2, false),
+    T011_10L("T011|1xL", 4, 15.toByte(), 4, 2, false),
+    K008_01("K008|0x", 4, 15.toByte(), 4, 2, false),
+    K008_10("K008|1x", 4, 15.toByte(), 4, 2, false),
+    K009_00("K009|00", 8, 255.toByte(), 4, 2, false),
+    K009_10("K009|10", 8, 255.toByte(), 4, 2, false),
+    K006_10("K006|10", 8, 255.toByte(), 4, 2, true),  // Prototype only
+    K006_20("K006|20", 8, 255.toByte(), 4, 2, true);
 
     companion object {
         @JvmStatic
         fun fromVersionName(versionName: String?): HardwareType? {
-            val version = versionName?.replace('|', '.')
             for (type in entries) {
-                if (type.versionName == version) {
+                if (type.versionName == versionName) {
                     return type
                 }
             }
